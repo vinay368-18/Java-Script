0b178b736c0197bb8e7bc20c442b826885fbb310
Vinay Kumar <1nt24cs122.vinayakumar@nmit.ac.in>
2026-07-31 10:38:48 +0530

    passing objects to a function

--- FuctionsAndObj.js
+++ FuctionsAndObj.js
@@ -1,9 +1,9 @@
-function student(name,dept){
+function student(name, dept) {
     this.name = name;
     this.dept = dept;
 }
-let s1 = new student("vinay","cse")
-let s2 = new student("arjun","mech")
+let s1 = new student("vinay", "cse")
+let s2 = new student("arjun", "mech")
 
 console.log(s1);
 console.log(s2);
