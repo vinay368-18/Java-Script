365a49bbd1ff7f5230977db589bb0406f142f69b
Vinay Kumar <1nt24cs122.vinayakumar@nmit.ac.in>
2026-07-31 10:54:32 +0530

    Arrays

--- Arrays.js
+++ Arrays.js
@@ -8,8 +8,14 @@ arr.push(20);
 arr.push(1);
 arr.push(2);
 console.log(arr);
-arr.pop(15); //removes elem 15
 arr.pop(); // removes last elem
 console.log(arr);
+arr.shift(); // removes first elem
+console.log(arr);
+arr.unshift(100) // adds elem to begining of array
+console.log(arr)
+console.log(arr.splice(1,2)) // 1-> index , 2-> number of elem from the index which need to be removed
+console.log(arr);
+
 
 
